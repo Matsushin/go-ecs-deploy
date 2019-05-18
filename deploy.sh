7d8c50cf9f22dafbef3bb86c332f5df12707cde1
Matsushin <matsu1983@gmail.com>
2019-05-18 16:27:22 +0900

    微修正

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 eval $(aws ecr get-login --no-include-email --region ap-northeast-1)
 docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-ecs-deploy-app:latest
-./scripts/ecs-deploy --cluster go-ecs-deploy-app-cluster --service-name go-ecs-deploy-app-service --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-ecs-deploy-app:latest --timeout 600
+./scripts/ecs-deploy --cluster go-ecs-deploy-cluster --service-name go-ecs-deploy-service --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-ecs-deploy-app:latest --timeout 600
